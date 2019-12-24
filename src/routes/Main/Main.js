8b43143a9fe6a3a3672546adead924a4353f5889
Michael Slinkin <michaelnslinkin@gmail.com>
2019-12-24 16:28:12 +0300

    added css for the main page

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Main() {
   return (
@@ -7,7 +9,10 @@ function Main() {
       <section>
         <nav>
           <div style={{ display: "flex", flexWrap: "wrap" }}>
-            <Button variant="primary" size="lg" title="Слинкин" to="/slinkin"/>
+          <Link to="/slinkin">
+            <Button variant="primary" size="lg" title="">Слинкин</Button>
+          </Link>  
+          
             <Button variant="primary" size="lg" title="Булгару" to="/slinkin"/>
             <Button variant="primary" size="lg" title="Мокрушин" to="/slinkin"/>
             <Button variant="primary" size="lg" title="Гайфулин" to="/slinkin"/>
