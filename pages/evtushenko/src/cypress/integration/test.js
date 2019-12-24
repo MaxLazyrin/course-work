55a6bcbd9cdd1f63e9de8e266800a35d364ef78c
maxim <evtushenko99@mail.ru>
2019-12-24 21:32:05 +0300

    Add all

---
+++ pages/evtushenko/src/cypress/integration/test.js
@@ -0,0 +1,27 @@
+describe('test', function () {
+  it('london', function () {
+    cy.visit('http://server:9000')
+
+    cy.get('#city').clear().type("London")
+    cy.get('#country').clear().type("GB")
+    cy.get('button').click()
+    cy.wait(500)
+    cy.get('#location').should('have.text', ' London, GB')
+  })
+  it('russia', function () {
+    cy.visit('http://localhost:9000')
+
+    cy.get('#city').clear().type("Moscow")
+    cy.get('#country').clear().type("RU")
+    cy.get('button').click()
+    cy.wait(500)
+    cy.get('#location').should('have.text', ' Moscow, RU')
+  })
+  it('error', function () {
+    cy.visit('http://localhost:9000')
+
+    cy.get('button').click()
+    cy.wait(500)
+    cy.get('#error').should('have.text', 'Please enter the values.')
+  })
+})
